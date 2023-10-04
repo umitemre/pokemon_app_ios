2060651696301559fa30214bdcc1fa9232fd41e1
umitemre <emreaydin149@gmail.com>
2023-10-04 12:12:49 +0300

    Initial commit

---
+++ Pokemon App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokemon App
+//
+//  Created by Emre Aydin on 4.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
