636635611c2987f94c8db676a529f1fffa4d0cfd
umitemre <emreaydin149@gmail.com>
2023-10-04 12:56:54 +0300

    Create base view controller

---
+++ Pokemon App/Scenes/BaseViewController.swift
@@ -0,0 +1,31 @@
+//
+//  BaseViewController.swift
+//  Pokemon App
+//
+//  Created by Emre Aydin on 4.10.2023.
+//
+
+import UIKit
+
+// MARK: StoryboardInstantiable
+protocol StoryboardInstantiable {
+    static func instance(fromName name: String) -> Self?
+}
+
+// MARK: BaseViewController
+class BaseViewController: UIViewController {
+
+}
+
+// MARK: StoryboardInstantiable
+extension BaseViewController: StoryboardInstantiable {
+    static func instance(fromName name: String) -> Self? {
+        let storyboard = UIStoryboard(name: name, bundle: nil)
+        return instance(fromStoryboard: storyboard)
+    }
+
+    private static func instance(fromStoryboard storyboard: UIStoryboard) -> Self? {
+        let a = String(describing: Self.self)
+        return storyboard.instantiateViewController(identifier: String(describing: Self.self)) as? Self
+    }
+}
---
+++ Pokemon App/Scenes/SplashViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SplashViewController.swift
+//  Pokemon App
+//
+//  Created by Emre Aydin on 4.10.2023.
+//
+
+import Foundation
+
+class SplashViewController: BaseViewController {
+    static func instance() -> SplashViewController? {
+        return instance(fromName: "SplashViewController")
+    }
+}
