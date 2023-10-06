35f819570ab403fb081f2f28a3d276a696b7133b
umitemre <emreaydin149@gmail.com>
2023-10-06 18:35:06 +0300

    Write an example test for search controller

--- Pokemon AppTests/SearchViewControllerTest.swift
+++ Pokemon AppTests/SearchViewControllerTest.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+import XCTest
+import RxSwift
+@testable import Pokemon_App
+
+class SearchViewControllerTest: XCTestCase {
+    private let disposeBag = DisposeBag()
+
+    var viewModel: SearchViewModelInput?
+    var repository: CardRepository?
+    
+    func test_WhenAPIRequestSucceed_CardResultShouldEmitted() {
+        let expectation = XCTestExpectation(description: "CardsResult")
+
+        guard let cardsResult = MockDataManager.getData(from: "mock_cards_result", type: CardsResult.self) else {
+            // Fail the test, as the JSON file can not be parsed into CardsResult
+            XCTAssertTrue(false)
+            return
+        }
+
+        repository = CardRepositoryMock(cardsResult)
+        viewModel = SearchViewModel(repository: repository!)
+
+        viewModel?.searchResultDidChange.subscribe { result in
+            guard let data = result.element else {
+                fatalError("No data returned, please check test.")
+            }
+            
+            XCTAssertNotNil(data.cards)
+            XCTAssertGreaterThan(data.cards?.count ?? 0, 0)
+            expectation.fulfill()
+        }.disposed(by: disposeBag)
+        
+        viewModel?.fetchSearchResults(hp: 0)
+
+        wait(for: [expectation], timeout: 5.0)
+    }
+}
